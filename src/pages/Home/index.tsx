d927dddc8a410341082685978792752968afab03
Joao Gomes <joaopfg.2002@gmail.com>
2021-05-07 12:23:03 +0100

    nothing important to commit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,9 +25,9 @@ const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
   const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-      
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+        
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
@@ -60,7 +60,7 @@ const Home = (): JSX.Element => {
         >
           <div data-testid="cart-product-quantity">
             <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0}  */}
+            {cartItemsAmount[product.id] || 0} 
           </div>
 
           <span>ADICIONAR AO CARRINHO</span>
